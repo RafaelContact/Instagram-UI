3f32ae45efa5d1aad68c00f631270049421d99f6
RafaelContact <contactrodrigues21@gmail.com>
2022-07-15 17:19:47 -0300

    removendo função desnecessária

--- js/inputs.js
+++ js/inputs.js
@@ -2,7 +2,7 @@ var pwd = window.document.getElementById('pwd')
 var value = pwd.value
 var show = window.document.getElementById('show')
 var submit = window.document.getElementById('submit')
-var email = window.document.getElementById('email')
+var email = window.document.getElementById('email').value
 
 document.getElementById("show").addEventListener("click", function(e){
     e.preventDefault()
@@ -25,24 +25,17 @@ function change(){
 
 pwd.addEventListener('input', keep)
 
-function keep(e){ // Está funcionando, mas não funciona no carregamento da página, só funciona quando o número fica zerado após apagar tudo
-    var x = e.target.value
-    if(x.length == 0 ){
-        show.innerHTML = ''
-    }else{
-        show.innerHTML = "Mostrar"
-        var aray = []
-        aray.push(x)
-        console.log(aray) // por que ele não cria outros elementos ? 
-    } 
-    }
-    if(x.length < 5){ // como fazer funcionar só carregando algum valor no email ?
-        submit.style.background = '#b2dffc'
-        //inserir também um prevent defalt
-    }else{
-        submit.style.background = '#0095F6'
-    }
-
-function loading(){
-    show.innerHTML = ''
-}
+function keep(e){ //agora funcionando com o carregamento da página
+        var x = e.target.value
+        if(x.length == 0 ){
+            show.innerHTML = ''
+        }else{
+            show.innerHTML = "Mostrar"
+        } 
+        if(x.length < 5){ // como fazer funcionar só carregando algum valor no email ?
+            submit.style.background = '#b2dffc'
+            //inserir também um prevent defalt
+        }else{
+            submit.style.background = '#0095F6'
+        }
+        }
